b3b6cba031633cc8b07e366bc380390f8a4901e1
Marin Marković <marin.sokol@gmail.com>
2020-02-06 22:33:29 +0100

    added tslint

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,9 @@ const dotenv = require('dotenv')
 
 module.exports = () => {
   const env = dotenv.config().parsed
-
-  const envKeys = Object.keys(env).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(env[next])
-    return prev
+  const envKeys = Object.keys(env).reduce((result, next) => {
+    result[`process.env.${next}`] = JSON.stringify(env[next])
+    return result
   }, {})
 
   return {
