cc365d4f7a063e54b61991e83a841255190ad5a4
Marin Marković <marin.sokol@gmail.com>
2020-02-06 19:12:52 +0100

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+import { Hello } from './Home'
+
+ReactDOM.render(<Hello compiler="TypeScript" framework="React" />, document.getElementById('root'))
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader'
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  }
+}
