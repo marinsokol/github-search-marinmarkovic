257704ce322f72af2fc86a490ffcc6c5c0698b83
Marin Marković <marin.sokol@gmail.com>
2020-02-07 19:12:58 +0100

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,20 @@
 import * as React from 'react'
-import { useQuery } from '@apollo/react-hooks'
-import { gql } from 'apollo-boost'
-import Card from 'react-bootstrap/Card'
-
-interface ViewerData {
-  viewer: { login: string; name: string }
-}
-
-const VIEWER = gql`
-  {
-    viewer {
-      login
-      name
-    }
-  }
-`
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './routes/Home'
+import User from './routes/User'
 
 const App = () => {
-  const { loading, error, data } = useQuery<ViewerData, {}>(VIEWER)
-
-  if (loading) {
-    return <p>Loading...</p>
-  }
-  if (error) {
-    return <p>{error.message}</p>
-  }
-
   return (
-    <Card>
-      <Card.Body>
-        <Card.Title>App: {data.viewer.login}</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the bulk of the card's content.
-        </Card.Text>
-      </Card.Body>
-    </Card>
+    <Router>
+      <Switch>
+        <Route path="/user">
+          <User />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
---
+++ src/routes/User/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const User = () => {
+  return (
+    <div>
+      USER
+      <Link to="/">Home</Link>
+    </div>
+  )
+}
+
+export default User
